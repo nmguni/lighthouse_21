9cbe37fab5ab4cbc25d67a4c2b8500937db947bb
Ndumiso Mguni <Ndumiso29@gmail.com>
2019-11-03 17:45:27 -0500

    first commit

---
+++ scripts/day1.js
@@ -0,0 +1,28 @@
+const volunteers = [
+    'Sally',
+    'Jake',
+    'Brian',
+    'Hamid'
+];
+
+
+const neighbourhoods = [
+    'Central Valley',
+    'Big Mountain',
+    'Little Bridge',
+    'Bricktown',
+    'Brownsville',
+    "Paul's Boutique",
+    'Clay Park',
+    'Fox Nest'
+];
+
+
+
+const doorToDoor = (volunteers, neighbourhoods) => {
+    let total = neighbourhoods.length / volunteers.length
+    return total
+
+}
+
+
