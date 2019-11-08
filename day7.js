d06eb4093a19b6bc95663acc5f468e1c0d25db32
Ndumiso Mguni <Ndumiso29@gmail.com>
2019-11-08 10:51:01 -0500

    day 7 started...will continue later

---
+++ day7.js
@@ -0,0 +1,31 @@
+smartGarbage(trash, bins)
+
+// trash  - string that tells what type of item is being submtted 
+// bins - object containing 3 props (waste, recycling, and compost) alos holds neumerical value 
+// func must increase the correct value in bins obj
+
+// input 
+const bins = {
+    waste: 4,
+    recycling: 2,
+    compost: 5
+}
+
+const trash = 'recycling'
+// output 
+{
+    waste: 4,
+        recycling: 3,
+            compost: 5
+}
+
+// obj.keys will make array of obj's propertes 
+// loop thru props 
+// use if to match trash with obj
+
+// read string and increase object 
+const smartGarbage = (trash, bins) => {
+    //   l
+
+}
+
