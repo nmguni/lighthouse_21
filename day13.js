f5135ec3140db0d0c74145626f88a59765b236a6
Ndumiso Mguni <Ndumiso29@gmail.com>
2019-11-12 09:56:36 -0500

    day 13, 1 failed test, will cont later

---
+++ day13.js
@@ -0,0 +1,39 @@
+const samples = [
+    "clean",
+    "clean",
+    "dirty",
+    "clean",
+    "dirty",
+    "clean",
+    "clean",
+    "dirty",
+    "clean",
+    "dirty"
+];
+
+const threshold = 0.3;
+
+const checkAir = function (samples, threshold) {
+    let cleanCount = [];
+    let dirtyCount = [];
+    let results = samples.filter(x => {
+        if (x == "clean") {
+            cleanCount.push(x);
+        } else if (x == "dirty") {
+            dirtyCount.push(x);
+        }
+    });
+    // find %
+    let percentage = dirtyCount.length / cleanCount.length;
+    // check if % is < or > then threshold
+    if (percentage > threshold) {
+        return "Polluted";
+    } else if (percentage < threshold) {
+        return "Clean";
+    }
+};
+
+checkAir(samples, threshold);
+
+// failed test
+//   Failed: If the ratio of dirty samples is below the threshold, return "Clean"
